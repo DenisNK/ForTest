d72c03f1c14984c2e4f6744253dd807260c73326
Denis Yurchenko <denis3101996@gmail.com>
2021-09-15 15:54:49 +0300

    Add dev branch 3

--- TestForGit/Program.cs
+++ TestForGit/Program.cs
@@ -9,6 +9,7 @@ namespace TestForGit
             Console.WriteLine("Hello World!");
             Console.WriteLine("add Develop branch");
             Console.WriteLine("add Develop branch2");
+            Console.WriteLine("add Develop branch3");
             Console.ReadKey();
         }
     }
