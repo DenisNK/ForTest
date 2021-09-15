7984ab2176f8e3ef2d7f64456aa797d91ae5189f
Denis Yurchenko <denis3101996@gmail.com>
2021-09-15 15:52:11 +0300

    Delete files
    Merge pull request #2 from DenisNK/DV-889
    
    Dv 889
    
    Merge pull request #2 from DenisNK/DV-889
    
    Dv 889
    
    Add newClass
    
    add devClass
    
    Develop
    
    Console.WriteLine("DV-888"); - 2
    
    Console.WriteLine("DV-888");
    
    Add dev branch 3
    
    Add dev branch 2
    
    Add dev branch
    
    Add project files.

---
+++ TestForGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("add Develop branch");
+            Console.WriteLine("add Develop branch2");
+            Console.WriteLine("add Develop branch3");
+            Console.WriteLine("DV-888");
+            Console.WriteLine("DV-888 - 2");
+ 			Console.WriteLine("Develop");
+            Console.WriteLine("DV-889");
+            Console.ReadKey();
+        }
+    }
+}
